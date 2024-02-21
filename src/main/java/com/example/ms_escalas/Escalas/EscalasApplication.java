e0ca5448bf3a6f833e6084a12ab75fb54cb0bc67
Gabriel Augusto <xqgabriel3@gmail.com>
2024-02-21 18:36:19 -0300

    #First Commit

---
+++ src/main/java/com/example/ms_escalas/Escalas/EscalasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_escalas.Escalas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ms_escalas/Escalas/EscalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_escalas.Escalas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
